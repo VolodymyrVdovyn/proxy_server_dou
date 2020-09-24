ca096b206532de0411f301517dff25e9abc91a9d
Volodymyr Vdovyn <volodymyr.vdovyn@gmail.com>
2020-09-24 13:57:03 +0300

    Update test_replacer.py

--- test_replacer.py
+++ test_replacer.py
@@ -9,25 +9,25 @@ def test_replace_links():
 
 
 @pytest.mark.xfail(reason="Reason: not tag in content")
-def test_modify_content_xfail():
+def test_replace_text_xfail():
     content = 'sixsix sevense six_si six111 Свежее'
     new_content = replacer.modify_content(content)
     assert new_content == 'sixsix™ sevense six_si six111 Свежее™'
 
 
-def test_modify_content_1():
+def test_replace_text_1():
     content = '<p>sixsix sevense six_si six111 Свежее</p>'
     new_content = replacer.modify_content(content)
     assert new_content == '<p>sixsix™ sevense six_si six111 Свежее™</p>'
 
 
-def test_modify_content_2():
+def test_replace_text_2():
     content = '<p>Всем привет. Случилась очень неприятная ситуация, и&nbsp;данной темой хотел предупредить остальных об&nbsp;очередных новых идеях этого оператора о&nbsp;том как нагреть.</p>'
     new_content = replacer.modify_content(content)
     assert new_content == '<p>Всем привет™. Случилась очень неприятная ситуация, и&nbsp;данной™ темой хотел предупредить остальных об&nbsp;очередных новых идеях этого оператора о&nbsp;том как нагреть.</p>'
 
 
-def test_replaser():
+def test_modify_content():
     content = '<p>sixsix sevense six_si six111 Свежее</p><a href="https://dou.ua/lenta/">Лента</a>'
     new_content = replacer.modify_content(content)
     assert new_content == '<p>sixsix™ sevense six_si six111 Свежее™</p><a href="http://127.0.0.1:8888/lenta/">Лента</a>'
